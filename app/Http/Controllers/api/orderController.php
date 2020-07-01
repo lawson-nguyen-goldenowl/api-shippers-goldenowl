9eaeab7f41d07ae59ab8237cd566bab6d3a7894e
trungnt98 <nguyenthanhtrung.1381998@gmail.com>
2020-07-01 12:09:57 +0700

    Isert orders

--- app/Http/Controllers/api/orderController.php
+++ app/Http/Controllers/api/orderController.php
@@ -7,12 +7,21 @@ use Illuminate\Http\Request;
 use App\orders as Order;
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use App\permission as Permission;
 
 class orderController extends Controller
 {
     //
-    public function create(Request $request){
-        $validator = Validator::make($request->all(),
+    public function create(Request $request)
+    {
+        $user = Auth::user();
+        if ($user->permission != Permission::where('title', 'admin')->get()->id) {
+            return response()->json([
+                'error' => 'Forbidden'
+            ], 403);
+        }
+        $validator = Validator::make(
+            $request->all(),
             [
                 'name' => 'required|unique:orders',
                 'weight' => 'required|numeric',
@@ -25,11 +34,26 @@ class orderController extends Controller
             return response()->json(
                 [
                     'error' => $validator->errors()
-                ], 400);
+                ],
+                400
+            );
         }
 
         $order = new Order;
-        $user = Auth::user();
-        return response()->json($user);
+        $order->name = $request->name;
+        $order->weight = $request->weight;
+        $order->recipientName = $request->recipientName;
+        $order->recipientPhone = $request->recipientPhone;
+        $saved = $order->save();
+        if (!$saved) {
+            return response()->json([
+                'error' => 'INTERNAL SERVER ERROR'
+            ], 500);
+        }
+        return response()->json([
+            'success' => [
+                'order' => $order
+            ]
+        ], 200);
     }
 }
