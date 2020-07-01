798ed677cc4cdd3336a3269faaa42aec6604dd36
trungnt98 <nguyenthanhtrung.1381998@gmail.com>
2020-07-01 11:34:32 +0700

    user response

--- app/Http/Controllers/api/orderController.php
+++ app/Http/Controllers/api/orderController.php
@@ -4,11 +4,31 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\orders as Order;
+use Illuminate\Support\Facades\Auth;
 
 class orderController extends Controller
 {
     //
     public function create(Request $request){
-        return response()->json($request);
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required|unique:orders',
+                'weight' => 'required|numeric',
+                'recipientName' => 'required',
+                'recipientPhone' => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'error' => $validator->errors()
+                ], 400);
+        }
+
+        $order = new Order;
+        $user = Auth::user();
+        return response()->json($user);
     }
 }
