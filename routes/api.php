c77e888a14e379f550d900b1f65479912c86a02a
trungnt98 <nguyenthanhtrung.1381998@gmail.com>
2020-07-01 10:11:39 +0700

    edit api routes

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'api\userController@login');
-Route::post('register', 'api\userController@register');
+Route::namespace('api')->group(function () {
+    Route::post('login', 'userController@login');
+    Route::post('register', 'userController@register');
+});
 
-Route::group(['middleware' => 'auth:api'], function () {
+Route::group(['middleware' => 'auth:api'], function() {
     Route::get('details', 'api\userController@details');
 });
