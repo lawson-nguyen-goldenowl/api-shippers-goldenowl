da822b16369f36f59ecaa16ee1a234e906fba757
trungnt98 <nguyenthanhtrung.1381998@gmail.com>
2020-07-01 11:38:28 +0700

    user response

--- app/Http/Controllers/api/orderController.php
+++ app/Http/Controllers/api/orderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\orders as Order;
 use Illuminate\Support\Facades\Auth;
+use Validator;
 
 class orderController extends Controller
 {
