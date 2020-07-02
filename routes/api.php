f53a2185c226be1bdb4cbb83d539ad7f875d4db8
trungnt98 <nguyenthanhtrung.1381998@gmail.com>
2020-07-02 18:01:43 +0700

    add api route update order

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::namespace('api')->group(function () {
 
 Route::group(['middleware' => 'auth:api', 'namespace' => 'api'], function() {
     Route::get('details', 'userController@details');
-    Route::group(['middleware' => 'permission'], function () {
+    Route::group(['middleware' => ['auth:api', 'permission'], function () {
         Route::get('orders', 'orderController@all');
 
         Route::post('orders', 'orderController@create');
